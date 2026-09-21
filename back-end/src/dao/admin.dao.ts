cbcdea4ebf9712c4b46a09cb38b7e7a4cc30a802
Alerrandro Lima Maia <elalerrandroads@gmail.com>
2026-09-21 10:42:57 -0300

    Teste com o registro

--- back-end/src/dao/admin.dao.ts
+++ back-end/src/dao/admin.dao.ts
@@ -5,7 +5,7 @@ export class AdminDAO {
     public async register(admin: Admin): Promise<void> {//Alterar quando criar o banco de dados, para ficar de acordo com a tabela
         try {
             const [result]: any = await connection.query(
-                'INSERT INTO administrator (id, name, email, hashPassword, role, active, createdAt) VALUES (?, ?, ?, ?)',
+                'INSERT INTO administrator (id, name, email, hashPassword, role, active, createdAt) VALUES (?, ?, ?, ?, ?, ?, ?)',
                 [admin.id, admin.name, admin.email, admin.password, admin.role, admin.active, admin.createdAt]
             );
         } catch (error: any) {
--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import routes from './routes/admin.routes';
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
 app.listen(8080, () => {
     console.log('Server is running on port 8080');
